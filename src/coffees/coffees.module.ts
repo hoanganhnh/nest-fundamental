1d142f6391edde8c33fef40badfd144e49088ffe
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-07-27 09:33:20 +0700

    custom provider & value base provider

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -10,7 +10,12 @@ import { Flavor } from './entities/flavor.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    {
+      provide: CoffeesService,
+      useClass: CoffeesService,
+    },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
