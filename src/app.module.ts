691ad9fd58a1c7685814efcb3f8f6fc62e84b9c8
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-07-27 10:02:49 +0700

    class providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -17,6 +18,9 @@ import { CoffeesModule } from './coffees/coffees.module';
       database: 'postgres',
       autoLoadEntities: true,
       synchronize: true,
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
     }),
   ],
   controllers: [AppController],
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -8,11 +8,22 @@ import { Event } from './entities/event.entity';
 import { Flavor } from './entities/flavor.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
+class ConfigService {}
+class DevConfigService {}
+class ProConfigService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevConfigService
+          : ProConfigService,
+    },
     {
       provide: COFFEE_BRANDS,
       useValue: ['nestcafe', 'trung nguyen'],
