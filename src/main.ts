f7f1c6288795139e7652d68ebe6959d3a902eef7
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-07-27 16:47:41 +0700

    catch exception with filters

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,8 @@ async function bootstrap() {
       },
     }),
   );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(5000);
 }
 
